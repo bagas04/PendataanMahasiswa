4c13ccea6be130217744b6bd7e278d85364c4ddf
bagas04 <bagasbonek89@gmail.com>
2020-11-12 16:19:59 +0700

    revisi

--- src/pendataanmahasiswa/PendataanMahasiswa.java
+++ src/pendataanmahasiswa/PendataanMahasiswa.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Date;
 public class PendataanMahasiswa {
-    static ArrayList <bagasputra07157_mahasiswaEntity> dataMahasiswa = new ArrayList();
-    static Scanner bagas07157_input = new Scanner (System.in);
-    static int pil, jurusan;
-    static String npm, nama, notelp, password, alamat;
-    static Date tglLahir;
+     ArrayList <bagasputra07157_mahasiswaEntity> dataMahasiswa = new ArrayList();
+     Scanner bagas07157_input = new Scanner (System.in);
+    
    
     public static void main(String[] args) {
+        int pil, jurusan;
+      //  String npm, nama, notelp, password, alamat;
+        //Date tglLahir;
+        PendataanMahasiswa Dataku = new PendataanMahasiswa();
         System.out.println("Selamat Datang Diaplikasi Pendataan Mahasiswa");
         do{
             System.out.println("1. Pendaftaran Mahasiswa");
@@ -19,50 +21,50 @@ public class PendataanMahasiswa {
             System.out.println("4. Hapus data Mahasiswa");
             System.out.println("5. Exit");
             System.out.print("Masukkan angka yang dipilih = ");
-            pil = bagas07157_input.nextInt();
+            pil = Dataku.bagas07157_input.nextInt();
             switch(pil) {
                 case 1 : 
-                    Pendaftaran();
+                    Dataku.Pendaftaran();
                     break;
                 case 2 : 
-                    LihatDataMahasiswa();
+                    Dataku.LihatDataMahasiswa();
                     break;
                 case 3 : 
-                    EditDataMahasiswa();
+                    Dataku.EditDataMahasiswa();
                      break;
                 case 4 : 
-                    HapusDataMahasiswa();
+                    Dataku.HapusDataMahasiswa();
                      break;
             }
         }while(pil != 5);
     }
     
-    static void Pendaftaran() {
+     void Pendaftaran() {
         System.out.println("=============||============");
         System.out.print("Input NPM = ");
-        npm = bagas07157_input.next();
+       String npm = bagas07157_input.next();
         System.out.print("Input Nama = ");
-        nama = bagas07157_input.next();
+       String nama = bagas07157_input.next();
         System.out.print("Input Tanggal Lahir = ");
-        tglLahir = new Date(bagas07157_input.next());
+       Date tglLahir = new Date(bagas07157_input.next());
         System.out.print("Input No.Telp = ");
-        notelp = bagas07157_input.next();
+       String notelp = bagas07157_input.next();
         System.out.print("Input Alamat = ");
-        alamat = bagas07157_input.next();
+       String alamat = bagas07157_input.next();
         System.out.print("Input Password = ");
-        password = bagas07157_input.next();
+       String password = bagas07157_input.next();
         for (int i = 0; i < bagasputra07157_jurusanEntity.jurusan.length; i++) {
             System.out.println(i+". "+bagasputra07157_jurusanEntity.jurusan [i]);
             }
             System.out.print("Pilih Jurusan = ");
-            jurusan = bagas07157_input.nextInt();
+           int jurusan = bagas07157_input.nextInt();
             dataMahasiswa.add(new bagasputra07157_mahasiswaEntity(npm, nama, password,
                     notelp, alamat,jurusan,tglLahir));
             System.out.println("==============||==============");
             System.out.println("Data Berhasil Ditambahkan");
             
         }
-     static void LihatDataMahasiswa(){
+     void LihatDataMahasiswa(){
          for(int i = 0; i < dataMahasiswa.size(); i++){
              System.out.println(i+". "+dataMahasiswa.get(i).getNpm()+","+
                      dataMahasiswa.get(i).getNama()+","+ bagasputra07157_jurusanEntity.jurusan[
@@ -70,7 +72,7 @@ public class PendataanMahasiswa {
              
          }
      }
-     static void EditDataMahasiswa(){
+      void EditDataMahasiswa(){
          for (int i = 0; i < dataMahasiswa.size(); i++) {
              System.out.println(i+". "+dataMahasiswa.get(i).getNpm()+" "+
                      dataMahasiswa.get(i).getNama()+""+ bagasputra07157_jurusanEntity.jurusan[
@@ -79,7 +81,7 @@ public class PendataanMahasiswa {
              
          }
          System.out.println("Pilih Angka Untuk Edit Data : ");
-         pil = bagas07157_input.nextInt();
+        int pil = bagas07157_input.nextInt();
          for (int i = 0; i < dataMahasiswa.size(); i++) {
              System.out.println("============||===============");
              System.out.println("1. NPM : "+dataMahasiswa.get(i).getNpm());
@@ -95,37 +97,37 @@ public class PendataanMahasiswa {
                 case 1 : 
                      System.out.println("NPM Sekarang : "+dataMahasiswa.get(i).getNpm());
                      System.out.print("Masukkan NPM Baru : ");
-                     npm = bagas07157_input.next();
+                     String npm = bagas07157_input.next();
                      dataMahasiswa.get(i).npm_mahasiswa = npm;
                      break;
                 case 2 :
                      System.out.println("Nama Sekarang : "+dataMahasiswa.get(i).getNama());
                      System.out.print("Masukkan Nama Baru : ");
-                     nama = bagas07157_input.next();
+                     String nama = bagas07157_input.next();
                      dataMahasiswa.get(i).nama_mahasiswa = nama;
                      break;
                 case 3 :
                      System.out.println("Tanggal Lahir Sekarang : "+dataMahasiswa.get(i).getTanggallahir());
                      System.out.print("Masukkan Tanggal Lahir Baru : ");
-                     tglLahir = new Date (bagas07157_input.next());
+                     Date tglLahir = new Date (bagas07157_input.next());
                      dataMahasiswa.get(i).tanggallahir = tglLahir;
                      break;
                 case 4 :
                      System.out.println("NO.Telp Sekarang : "+dataMahasiswa.get(i).getNotelp());
                      System.out.print("Masukkan NO.Telp Baru : ");
-                     notelp = bagas07157_input.next();
+                     String notelp = bagas07157_input.next();
                      dataMahasiswa.get(i).notelp_mahasiswa = notelp;
                      break;
                 case 5 :
                      System.out.println("Alamat Sekarang : "+dataMahasiswa.get(i).getAlamat());
                      System.out.print("Masukkan Alamat Baru : ");
-                     alamat = bagas07157_input.next();
+                     String alamat = bagas07157_input.next();
                      dataMahasiswa.get(i).alamat_mahasiswa = alamat;
                      break;
                 case 6 :
                      System.out.println("Password Sekarang : "+dataMahasiswa.get(i).getPassword());
                      System.out.print("Masukkan Password Baru : ");
-                     password = bagas07157_input.next();
+                     String password = bagas07157_input.next();
                      dataMahasiswa.get(i).password = password;
                      break;
                 case 7 :
@@ -135,7 +137,7 @@ public class PendataanMahasiswa {
                     }
                      System.out.println("Jurusan Sekarang : "+bagasputra07157_jurusanEntity.jurusan[dataMahasiswa.get(i).getJurusan()]);
                      System.out.print("Pilih Jurusan : ");
-                     jurusan = bagas07157_input.nextInt();
+                     int jurusan = bagas07157_input.nextInt();
                      dataMahasiswa.get(i).jurusan_mahasiswa = jurusan;
                      break;
                     }
@@ -144,14 +146,14 @@ public class PendataanMahasiswa {
          }
      }
      
-     static void HapusDataMahasiswa(){
+      void HapusDataMahasiswa(){
          for (int i = 0; i < dataMahasiswa.size(); i++) {
              System.out.println(i+". "+dataMahasiswa.get(i).getNpm()+""+dataMahasiswa.get(i).getNama()
              +""+bagasputra07157_jurusanEntity.jurusan[dataMahasiswa.get(i).getJurusan()]);
              
          }
          System.out.print("Pilih angka untuk hapus data : ");
-         pil = bagas07157_input.nextInt();
+         int pil = bagas07157_input.nextInt();
          dataMahasiswa.remove(dataMahasiswa.get(pil));
          System.out.println("data berhasil dihapus :");
      }
